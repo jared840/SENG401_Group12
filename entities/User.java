4b6800067eafecaadcaf4895b24466a0d0f86958
Labib <labibafsarahmed02@gmail.com>
2023-02-13 19:42:05 -0700

    Adding User Entity

---
+++ entities/User.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class User{
+    private String firstName;
+    private String lastName;
+    private String userAddress;
+    private String userEmail;
+    private String password;
+    private String cardNumber;
+
+
+    public User(String firstName, String lastName, String userAddress, String userEmail, String password,
+            int cardNumber) {
+    }
+    
+}
+
