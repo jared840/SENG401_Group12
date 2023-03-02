322163e2d43a97ae1ce3d9bf9dbf7859ad3c556a
jordan427-prog <jordanlundy427@gmail.com>
2023-03-01 22:57:22 -0700

    wwt temp

---
+++ UnitTests/WarehouseWorkerTests.java
@@ -0,0 +1,17 @@
+package UnitTests;
+
+
+
+import entities.*;
+
+import java.util.List;
+
+import org.junit.*;
+import org.junit.Assert.*;
+
+public class WarehouseWorkerTests {
+    public WarehouseWorkerTests()
+    {
+        //do nothing
+    }
+}
