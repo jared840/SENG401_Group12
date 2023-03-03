6b8f15f337e3857b068930ded5354a8b9ab41777
jared840 <81462044+jared840@users.noreply.github.com>
2023-03-02 18:44:59 -0700

    Added more unit tests

--- UnitTests/UserTests.java
+++ UnitTests/UserTests.java
@@ -29,14 +29,31 @@ public class UserTests {
 
 /**
  * Test case: testing the functionality of adding a product to a user's cart
+ * Thus, tests both the add to cart functionality, as well as the getter for the user's cart!
+ * Assert: Asserts that the sample test product is in fact contained within the user's cart
  */
     @Test 
     public void addToCartTest(){
+        System.out.println("Testing User add to cart functionality ....");
         tester.addProductToCart(testProduct);
 
-        //Assert.assertEquals
+        Assert.assertTrue(tester.getCartProducts().contains(testOrder));
     }
 
+    /**
+     * Test case: testing ability to remove items from a user's cart
+     * Adds product to cart, then removes the product. Asserts that the user's cart does NOT contain the prodcut that was removed
+     */
+    @Test 
+    public void removeFromCartTest(){
+        System.out.println("Tersting cart removal for user...");
+        testProduct=new Product(1000, 678, "Hard Drive", "Computer external memory ", 250.50, "Hard Drives & Memory", 13);
+        tester.addProductToCart(testProduct);
+
+        tester.removeProductFromCart(testProduct);
+        Assert.assertFalse(tester.getCartProducts().contains(testOrder));
+
+    }
 
 
     @After
