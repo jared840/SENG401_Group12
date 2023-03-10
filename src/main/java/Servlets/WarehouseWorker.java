844dcda7f3f09795e98524b231fcd2cf8879750f
Jordan Lundy <81479299+jordan427-prog@users.noreply.github.com>
2023-03-09 20:33:24 -0700

    Add files via upload
    added warehouse worker servlet

---
+++ src/main/java/Servlets/WarehouseWorker.java
@@ -0,0 +1,70 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WarehouseWorker
+ */
+@WebServlet("/WarehouseWorker")
+public class WarehouseWorker extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public WarehouseWorker() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+	
+	
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		String username=request.getParameter("usernameww");
+		String password=request.getParameter("passwordww");
+		String name=request.getParameter("nameww");
+	
+		try {
+			
+			DBController d= new DBController("jdbc:mysql://localhost:3306/SENG401Project", "root","password");
+			WarehouseWorkers w=new WarehouseWorkers(1,name,username,password);
+			d.newUser(w);
+			
+		}catch(Exception E)
+		{
+			out.println(E.getMessage());
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
