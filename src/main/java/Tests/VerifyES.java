9689d31c80d4324f16f08ff08255c406261272ad
Jared Lundy <81462044+jared840@users.noreply.github.com>
2023-03-28 05:30:15 -0600

    Add test

---
+++ src/main/java/Tests/VerifyES.java
@@ -0,0 +1,34 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import database.DBController;
+import junit.framework.Assert;
+
+public class VerifyES {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	//Verifies that event sourcing occurred
+	@Test
+	public void test() {
+		try {
+			DBController db = new DBController("jdbc:mysql://localhost:3306/SENG401Project", "root", "vick-newton7.1");
+			int es_no = db.getEventSourceNumebr();
+			Assert.assertTrue(es_no>0);
+		}catch(Exception e) {
+			fail();
+		}
+	}
+
+}
